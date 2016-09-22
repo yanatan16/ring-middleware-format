49e2f48f2f92928e9719e0566ab1530427dea193
Jon Eisen <jon@joneisen.me>
2016-09-22 16:04:33 -0600

    0.7.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.yanatan16/ring-middleware-format "0.7.1"
+(defproject org.clojars.yanatan16/ring-middleware-format "0.7.2-SNAPSHOT"
   :description "Ring middleware for parsing parameters and emitting
   responses in various formats (mainly JSON, YAML and Transit out of
   the box)"
